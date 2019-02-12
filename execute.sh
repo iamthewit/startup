d8c275a12d189703e71492c6e6a922092ce9bab5
Ben Coss <bencross86@gmail.com>
2019-02-12 19:03:18 +0000

    Changing to the directory of the scriptso that we can find the cfg files

--- execute.sh
+++ execute.sh
@@ -2,6 +2,8 @@
 
 # TODO: only execute these commands if it's a weekday and the time is before 6pm
 
+cd "$(dirname "$0")"
+
 if [ $(date +%u) -lt 6 ] ; then
     workday=$(cat workday.cfg)
     for line in $workday ;
